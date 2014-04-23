7e9f99dad2d79698be3e7e2a0828cb8d68bde678
Mustafa <mushk45@gmail.com>
2014-04-23 09:57:15 -0700

    draft of effect routing code

---
+++ rsc3/examples/routing.rkt
@@ -0,0 +1,15 @@
+#lang racket
+
+
+;; adds an effect to the end of the effect chain of the given track
+;;
+(define (append-effect track-num effect-ugen)
+  ; TODO
+  empty)
+
+;; creates a ugen that is routed
+;; in: input bus channel
+;; out: output bus channel
+(define (make-effect-ugen effect-ugen in out)
+  ; TODO
+  empty)
