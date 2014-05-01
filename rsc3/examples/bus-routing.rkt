c49cf584f79a2351a89f5b386e4f354c05e19fa7
Mustafa <mushk45@gmail.com>
2014-05-01 11:04:35 -0700

    added bus routing example usage

--- rsc3/examples/bus-routing.rkt
+++ rsc3/examples/bus-routing.rkt
@@ -10,13 +10,12 @@
        [sig (mul (sin-osc ar
                           (mul-add (lf-pulse ar 15 0 0.5) 200 freq)
                           0)
-                      (key-state kr 38 0 0.1 0.1))])
+                 (key-state kr 38 0 0.1 0.1))])
   (audition (out 16 sig)))
 
 (audition (out 0 (free-verb (in 1 ar 16) 0.5
                             (mouse-y kr 0 1 0 0.1)
                             0.5)))
-
 
 
 
