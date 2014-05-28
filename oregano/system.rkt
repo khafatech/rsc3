f546b393ab3e1e788c5cc5b3844a84639932f008
Mustafa <mushk45@gmail.com>
2014-05-27 18:01:07 -0700

    added running/checking scsynth

--- oregano/system.rkt
+++ oregano/system.rkt
@@ -7,9 +7,15 @@
 ;; run scsynth
 (define (run-super-collider)
   (match (system-type 'os)
-    ('unix (when (not (system "ps -e | grep scsynth > /dev/null"))
-             (thread (lambda ()
-                     (system "./start_server_linux.sh")))))
+    ('unix (if (system "ps -e | grep scsynth > /dev/null")
+               (display "SuperCollider Running\n")
+               (begin
+                 (display "Starting SuperCollider...")
+                 (process "./start_server_linux.sh")
+                 (sleep 0.3)
+                 (if (system "ps -e | grep scsynth > /dev/null")
+                     (display "OK")
+                     (display "Error")))))
     ('macosx 1)
     ('windows  1)
     (else 1)))
